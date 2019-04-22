d076099b17cda07e8d606449b57cab56bcb031ca
Abhinav Kushwaha <akush2007@gmail.com>
2019-04-22 09:31:39 +0530

    favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,11 +2,11 @@ const guid = process.env.NETLIFY_GOOGLE_ANALYTICS_ID;
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Serif',
+    title: 'Ipsator Analytics',
     description: 'my theme',
     contact: {
       phone: 'XXX XXX XXX',
-      email: 'zerostaticthemes@gmail.com',
+      email: 'info@ipsator.com',
     },
     menuLinks: [
       {
